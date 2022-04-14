08b02773c040f1cf616b4f7bf30d88b4374cd65e
Gabriel Mazurco <mazurco066@gmail.com>
2022-04-13 22:22:54 -0300

    build(2.2.3): build for 2.2.3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -100,7 +100,7 @@
       <div class="row">
         <div class="col-12">
           <p class="text-center">
-            Versão: <strong>2.2.2</strong>
+            Versão: <strong>2.2.3</strong>
           </p>
         </div>
       </div>
