2e01d6bd568de784c6017e0c754e89fac58db4ab
Gabriel Mazurco <mazurco066@gmail.com>
2022-05-23 01:40:43 -0300

    feat(profile): added a brand new profile design

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,23 +3,38 @@
   
     <!-- Page main content -->
     <div class="container">
-      <div class="row pt-3 primary-section ">
+      <div class="row">
         <div class="col-12">
           <h3 class="klasik">{{ $t('profile.title') }}</h3>
-          <p class="mb-3">{{ $t('profile.subtitle') }}</p>
-        </div>
-      </div>
-      <form @submit.prevent="saveProfile">
-        <div class="row secondary-section mb-3">
-          <div class="col-12 pt-3">
-            <div class="avatar">
-              <div class="icon">
-                <img src="/img/arts/face-recognition.png" alt="Sample user">
-              </div>
-            </div>
+          <div class="info">
+            <p class="mb-1">
+              <strong>{{ $t('profile.notice') }}</strong>
+            </p>
+            <p class="mb-0" v-html="$t('profile.subtitle')"/>
           </div>
         </div>
-        <div class="row mb-3">
+      </div>
+      <form class="profile-form" @submit.prevent="saveProfile">
+        <!-- TODO: Implement picture update -->
+        <div class="avatar">
+          <div class="image-container">
+            <img src="/img/j_black.jpg" alt="Avatar">
+            <button type="button">
+              <font-awesome-icon icon="camera" />
+            </button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <base-input
+              type="text"
+              addonLeftIcon="user"
+              :label="$t('profile.userField')"
+              :placeholder="$t('profile.userField')"
+              v-model="me.username"
+              disabled
+            />
+          </div>
           <div class="col-12">
             <base-input
               type="text"
@@ -35,27 +50,12 @@
           <div class="col-12">
             <base-input
               type="text"
-              addonLeftIcon="user"
-              :label="$t('profile.userField')"
-              :placeholder="$t('profile.userField')"
-              v-model="me.username"
-              disabled
-            />
-          </div>
-          <div class="col-12">
-            <base-input
-              type="text"
               addonLeftIcon="mobile"
               :label="$t('profile.accessField')"
               :placeholder="$t('profile.accessField')"
               :value="me.role === 'player' ? $t('profile.musician') : $t('profile.admin')"
               disabled
             />
-          </div>
-          <div class="col-12 mb-3 text-center">
-            <span>
-              {{ $t('profile.createdAt') }} <strong>{{ $text.formatISODate(new Date(parseInt(me.createdAt)).toISOString()) }}</strong>
-            </span>
           </div>
           <div class="col-12 mb-3">
             <base-button
@@ -66,7 +66,7 @@
               {{ $t('profile.submit') }}
             </base-button>
           </div>
-          <div class="col-12">
+          <div class="col-12 mb-3">
             <base-button
               nativeType="button"
               type="danger"
@@ -75,6 +75,11 @@
             >
               {{ $t('profile.logoff') }}
             </base-button>
+          </div>
+          <div class="col-12 text-center">
+            <span>
+              {{ $t('profile.createdAt') }} <strong>{{ $text.formatISODate(new Date(parseInt(me.createdAt)).toISOString()) }}</strong>
+            </span>
           </div>
         </div>
       </form>
