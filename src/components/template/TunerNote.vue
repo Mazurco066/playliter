01b457c9423d4c456826f97763dfac5ae173ecf7
Gabriel Mazurco <mazurco066@gmail.com>
2022-05-27 14:35:29 -0300

    feat(tuner) added css

--- src/components/template/TunerNote.vue
+++ src/components/template/TunerNote.vue
@@ -1,11 +1,8 @@
 <template>
-  <div
-    :class="'note mx-5 text-6xl font-bold relative inline-block ' + (active ? 'text-green-500' : '')"
-    @click="select"
-  >
+  <div :class="'note ' + (active ? 'text-primary' : '')" @click="select">
     {{ name[0] }}
-    <span class="absolute font-normal note-sharp text-3xl top-0">{{ name[1] || '' }}</span>
-    <span class="absolute font-normal text-lg opacity-70 bottom-0">{{ octave }}</span>
+    <span class="name">{{ name[1] || '' }}</span>
+    <span class="octave">{{ octave }}</span>
   </div>
 </template>
 
